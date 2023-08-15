37ab2133dd3cfb424902c1872181f3ca07fe0900
Hector Martinez <hmartinezmoreira81@gmail.com>
2023-08-14 23:23:57 -0600

    feat: constant color

---
+++ src/utils/constants/UI/colors/portfolioColor.ts
@@ -0,0 +1,43 @@
+export const COLORS_PORTFOLIO = {
+  uiPurple: {
+    uiPurple500: {
+      main: "#732CB1",
+      hover: "#672571",
+    },
+    uiPurple400: {
+      main: "#6945C0",
+      hover: "#5D3DB4",
+    },
+  },
+  uiBlue: {
+    uiBlue500: {
+      main: "#2B5AEE",
+      hover: "#254EDD",
+    },
+    uiBlue400: {
+      main: "#4B90EC",
+      hover: "#407AD9",
+    },
+  },
+  uiGreen: {
+    uiGreen500: {
+      main: "#11D6A2",
+      hover: "#10C292",
+    },
+    uiGreen700: {
+      main: "#1EB5A5",
+      hover: "#1A9F95",
+    },
+  },
+  uiBrown: {
+    uiBrown100: "#E0E0E0",
+    uiBrown200: "#B0B0B0",
+    uiBrown300: "#818181",
+    uiBrown400: "#666666",
+    uiBrown500: "#494949",
+    uiBrown600: "#363636",
+    uiBrown700: "#242424",
+    uiBrown800: "#121212",
+    uiBrown900: "#000000",
+  },
+};
