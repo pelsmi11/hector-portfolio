aa3389bd93158f042740f2578fe2121c88f52f5f
pelsmi11 <53068447+pelsmi11@users.noreply.github.com>
2023-10-06 23:13:42 -0600

    feat: eliminate text blue

--- src/features/informationHub/components/AboutSection/InformationAbout/InformationAbout.tsx
+++ src/features/informationHub/components/AboutSection/InformationAbout/InformationAbout.tsx
@@ -18,28 +18,26 @@ export const InformationAbout = () => {
       </div>
       <div className="mt-6 space-y-4">
         <div className="flex gap-8">
-          <p className="w-16 text-ui-secondary font-bold">{t("ABOUT.NAME")}</p>
+          <p className="w-16 font-bold">{t("ABOUT.NAME")}</p>
           <p className="font-bold text-base">:</p>
           <p>{t("ABOUT.MY_NAME")}</p>
         </div>
         <div className="flex gap-8">
-          <p className="w-16 text-ui-secondary font-bold">{t("ABOUT.PHONE")}</p>
+          <p className="w-16 font-bold">{t("ABOUT.PHONE")}</p>
           <p className="font-bold text-base">:</p>
           <Link href={"tel:+50257156731"}>
             <p>{t("ABOUT.MY_PHONE")}</p>
           </Link>
         </div>
         <div className="flex gap-8">
-          <p className="w-16 text-ui-secondary font-bold">{t("ABOUT.EMAIL")}</p>
+          <p className="w-16 font-bold">{t("ABOUT.EMAIL")}</p>
           <p className="font-bold text-base">:</p>
           <Link href={"mailto:hmartinezmoreira81@gmail.com"}>
             <p>{t("ABOUT.MY_EMAIL")}</p>
           </Link>
         </div>
         <div className="flex gap-8">
-          <p className="w-16 text-ui-secondary font-bold">
-            {t("ABOUT.FREELANCE")}
-          </p>
+          <p className="w-16 font-bold">{t("ABOUT.FREELANCE")}</p>
           <p className="font-bold text-base">:</p>
           <p>{t("ABOUT.MY_FREELANCE")}</p>
         </div>
