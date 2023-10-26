68a273d4935002d38f858b6869062abbc6c08a54
pelsmi11 <53068447+pelsmi11@users.noreply.github.com>
2023-10-25 22:23:13 -0600

    add new items

---
+++ src/utils/constants/assets/favicon/favicons.ts
@@ -0,0 +1,49 @@
+export const FAVICONS = [
+  {
+    rel: "android-chrome",
+    sizes: "192x192",
+    href: "/favicon_portfolio/android-chrome-192x192.png",
+  },
+  {
+    rel: "android-chrome",
+    sizes: "384x384",
+    href: "/favicon_portfolio/android-chrome-384x384.png",
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon_portfolio/apple-touch-icon.png",
+  },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon_portfolio/apple-touch-icon.png",
+  },
+  {
+    rel: "favicon",
+    sizes: "16x16",
+    href: "/favicon_portfolio/favicon-16x16.png",
+  },
+  {
+    rel: "favicon",
+    sizes: "32x32",
+    href: "/favicon_portfolio/favicon-32x32.png",
+  },
+  {
+    rel: "mstile",
+    sizes: "150x150",
+    href: "/favicon_portfolio/mstile-150x150.png",
+  },
+  {
+    rel: "icon",
+    href: "/favicon_portfolio/favicon.ico",
+  },
+  {
+    rel: "mask-icon",
+    href: "/favicon_portfolio/safari-pinned-tab.svg",
+  },
+  {
+    rel: "manifest",
+    href: "/favicon_portfolio/site.webmanifest",
+  },
+];
--- src/utils/constants/assets/index.ts
+++ src/utils/constants/assets/index.ts
@@ -1,2 +1,3 @@
 export * from "./images";
 export * from "./post";
+export * from "./favicon";
